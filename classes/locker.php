08fd875054bff66f155529c9eabd67ab4ff680ec
matthew murphy <mattyhead@gmail.com>
2016-04-28 09:55:41 -0400

    Adding docblocks

--- classes/locker.php
+++ classes/locker.php
@@ -1,24 +1,47 @@
 <?php
-
-class Locker
+/**
+ * Class Locker - prevent concurrent execution.
+ */
+class locker
 {
+    /**
+     * Determine if unlocked.
+     *
+     * @return bool True if unlocked, False otherwise.
+     */
     public function isUnlocked()
     {
         $check = json_decode(file_get_contents('status.json'));
-        return $check->status === "unlocked";
+
+        return $check->status === 'unlocked';
     }
 
-    public function lock($status = "locked")
+    /**
+     * Lock.
+     *
+     * @param string $status (description)
+     */
+    public function lock($status = 'locked')
     {
         $this->setStatus($status);
     }
 
-    public function unlock($status = "unlocked")
+    /**
+     * Unlock.
+     *
+     * @param string $status (description)
+     */
+    public function unlock($status = 'unlocked')
     {
         $this->setStatus($status);
     }
- 
-    public function setStatus($status)
+
+    /**
+     * Do the actual work for lock() and unlock().
+     *
+     * @param <type> $status (description)
+     */
+    private function setStatus($status)
     {
         file_put_contents('status.json', json_encode(array('status' => $status, 'time' => date(DATE_ATOM))));
     }
